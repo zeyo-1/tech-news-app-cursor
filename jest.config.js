913af3fa3da8bbc9529baceb649b329a5c5bdaec
Ryuma Sato <your.email@example.com>
2025-02-11 22:06:25 +0900

    chore: CI/CD・テスト基盤の整備
    - GitHub Actionsの設定ファイルを追加
    - Jestの設定を更新（config, setup）
    - テストファイルの追加（button.test.tsx）
    - READMEの更新（実装計画と技術仕様の詳細化）

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
